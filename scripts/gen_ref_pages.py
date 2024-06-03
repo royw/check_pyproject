84dcc453f3dc150883b8bc72f4bdca751fe58fea
Roy Wright <roy@wright.org>
2024-06-03 01:14:52 -0500

    document __main__.py files

--- scripts/gen_ref_pages.py
+++ scripts/gen_ref_pages.py
@@ -20,8 +20,8 @@ for path in sorted(src.rglob("*.py")):
         parts = parts[:-1]
         doc_path = doc_path.with_name("index.md")
         full_doc_path = full_doc_path.with_name("index.md")
-    elif parts[-1] == "__main__":
-        continue
+    # elif parts[-1] == "__main__":
+    #     continue
 
     nav[parts] = doc_path.as_posix()
 
